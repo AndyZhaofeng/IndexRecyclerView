210b865b27208943de59d0a4045c5bf3b6dfe50e
AndyZhaofeng <zf526637166@gmail.com>
2016-06-22 15:26:47 +0800

    绘制字母列表，添加触摸事件。

--- app/src/main/java/com/zfeng/tools/indexrecyclerview/widget/IndexRecyclerView.java
+++ app/src/main/java/com/zfeng/tools/indexrecyclerview/widget/IndexRecyclerView.java
@@ -2,9 +2,14 @@ package com.zfeng.tools.indexrecyclerview.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 
 /**
  * Created by zhaofeng on 16/6/21.
@@ -30,7 +35,7 @@ public class IndexRecyclerView extends RecyclerView
 
     private void init()
     {
-        indexView=new IndexView();
+        indexView=new IndexView(getContext(),this);
     }
 
     @Override
@@ -39,12 +44,43 @@ public class IndexRecyclerView extends RecyclerView
         {
             indexView.onDraw(c);
         }
+
         super.onDraw(c);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        indexView.onSizeChanged(w,h,oldw,oldh);
+        if(indexView!=null)
+        {
+            indexView.onSizeChanged(w,h,oldw,oldh);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent e) {
+        RectF rectIndex=indexView.getRectIndex();
+        int count=e.getPointerCount();
+        if(count==1)
+        {
+            if(e.getX()>rectIndex.left&&e.getX()<rectIndex.right
+                    &&e.getY()>rectIndex.top&&e.getY()<rectIndex.bottom)
+            {
+                return true;
+            }
+        }
+        return super.onInterceptTouchEvent(e);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        float height=e.getY()-indexView.getRectIndex().top;
+        int count=(int)Math.ceil(height/indexView.getInitStep());
+        if(count>0&&count<indexView.LetterList.length())
+        {
+            Log.d("IndexRecycler","count="+count+"  letter = "+indexView.LetterList.charAt(count-1));
+            return true;
+        }
+        return super.onTouchEvent(e);
     }
 }
--- app/src/main/java/com/zfeng/tools/indexrecyclerview/widget/IndexView.java
+++ app/src/main/java/com/zfeng/tools/indexrecyclerview/widget/IndexView.java
@@ -1,18 +1,100 @@
 package com.zfeng.tools.indexrecyclerview.widget;
 
+import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by zhaofeng on 16/6/21.
  */
 public class IndexView {
 
+    public static final String LetterList="ABCDEFGHIJKLMNOPQRSTUVWXYZ#";
+    private static final int base_integer=20;
+    private static final int base_textSize=12;
+    private Context context;
+    private RecyclerView recyclerView;
+    private float mDensity;             //当前屏幕密度除以160
+    private float mScaledDensity;       //当前屏幕密度除以160（设置字体的尺寸）
+    private float mAlphaRate;           //透明度
+
+    private int mRecyclerViewWidth;
+    private int mRecyclerViewHeight;
+    private RectF mIndexbarRect;
+    private int mIndexbarWidth;
+    private int mIndexbarHeight;
+    private int mIndexbarMarginTop;
+    private int mRecyclerViewMarginRight;
+    private float initStep;
+
+    public IndexView(Context context,RecyclerView recyclerView)
+    {
+        this.context=context;
+        this.recyclerView=recyclerView;
+        mDensity=context.getResources().getDisplayMetrics().density;
+        mScaledDensity=context.getResources().getDisplayMetrics().scaledDensity;
+
+        mIndexbarWidth=(int)(base_integer*mDensity);
+        mIndexbarMarginTop=(int)(base_integer*mDensity);
+        mAlphaRate=0.5f;
+
+
+    }
     public void onDraw(Canvas canvas)
     {
+        Paint indexbarPaint=new Paint();
+        indexbarPaint.setColor(Color.BLACK);
+        indexbarPaint.setAlpha(120);
 
+        //绘制索引条
+        canvas.drawRoundRect(mIndexbarRect,10,10,indexbarPaint);
+        drawLetters(canvas);
     }
     public void onSizeChanged(int w, int h, int oldw, int oldh)
     {
+        mRecyclerViewWidth=w;
+        mRecyclerViewHeight=h;
+        /**
+         * left top right bottom
+         */
+        mIndexbarRect=new RectF(mRecyclerViewWidth-mIndexbarWidth,mIndexbarMarginTop/2,
+                mRecyclerViewWidth,mRecyclerViewHeight-mIndexbarMarginTop/2);
+        initStep=mIndexbarRect.height()/27;
+    }
+    private void drawLetters(Canvas canvas)
+    {
+        Paint letterPaint=new Paint();
+        letterPaint.setColor(Color.WHITE);
+        float textSize=(int)mScaledDensity*base_textSize;
+        letterPaint.setTextSize(textSize);
 
+        float initTop=mIndexbarRect.top+textSize;
+        float initLeft=mIndexbarRect.left;
+        float letterTopMargin=2*mDensity;
+
+        for(int i=0;i<LetterList.length();++i)
+        {
+            Rect rect=new Rect();
+            letterPaint.getTextBounds(LetterList.charAt(i)+"",0,1,rect);
+            float margin=mIndexbarWidth-rect.width();
+            canvas.drawText(LetterList.charAt(i)+"",initLeft+margin/2,initTop+letterTopMargin,letterPaint);
+
+            initTop+=initStep;
+        }
+    }
+    public RectF getRectIndex()
+    {
+        if(mIndexbarRect!=null){
+            return mIndexbarRect;
+        }
+        return null;
+    }
+    public float getInitStep()
+    {
+        return initStep;
     }
 }
